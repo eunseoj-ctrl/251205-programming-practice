aa91fd3df7c6e80f58d9e2d97599c4549c3a5ad1
정은서 <eunseoj@gmail.com>
2025-12-07 10:59:02 +0900

    Sure! Pl

---
+++ env-config.js
@@ -0,0 +1,9 @@
+// 환경 변수 설정 파일
+// .env 파일의 VITE_OPENAI_API_KEY를 window 객체에 할당
+if (import.meta.env && import.meta.env.VITE_OPENAI_API_KEY) {
+  window.VITE_OPENAI_API_KEY = import.meta.env.VITE_OPENAI_API_KEY;
+}
+
+
+
+
